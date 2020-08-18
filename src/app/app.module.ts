0617c03c69fb1b97468a52b5fa72794df89dce4a
cpliu338 <cpliu338@yahoo.com>
2020-08-18 15:41:40 +0800

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
 
 // Import of the angular-material angular module
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
 
 @NgModule({
   imports: [ 
+    AppRoutingModule,
     BrowserModule,
     FormsModule,
-
+    HttpModule,
     // Import of the angular-material angular module
     AngularMaterialModule
   ],
